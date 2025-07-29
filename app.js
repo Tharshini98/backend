255ecb0b1394d9f05843a2268643a05cd73e8d5c
Tharshini98 <70389909+Tharshini98@users.noreply.github.com>
2025-07-29 21:10:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
